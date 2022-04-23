48b899aaa30b32aaa6b88819b2b88130cb784f4f
Brandon30067308 <bchikezie30@gmail.com>
2022-04-23 19:20:24 +0100

    update styles

--- src/components/Dashboard/DashboardContent/InfoCards/InfoCards.tsx
+++ src/components/Dashboard/DashboardContent/InfoCards/InfoCards.tsx
@@ -1,38 +1,46 @@
-import { FC } from 'react';
-import InfoCard from './InfoCard/InfoCard';
-import DashboardStyles from '../../Dashboard.module.scss';
-import img from '../../../../images/image-1.png';
-import { info } from '../../../../types/types';
-import useWindowDimensions from './../../../../hooks/useWindowDimensions';
-import breakpoints from './../../../../breakpoints/breakpoints';
+import { FC } from "react";
+import InfoCard from "./InfoCard/InfoCard";
+import DashboardStyles from "../../Dashboard.module.scss";
+import img from "../../../../images/image-1.png";
+import { info } from "../../../../types/types";
+import useWindowDimensions from "./../../../../hooks/useWindowDimensions";
+import breakpoints from "./../../../../breakpoints/breakpoints";
 
 interface InfoCardsProps {
-  info: info[]
+  info: info[];
 }
 
 const InfoCards: FC<InfoCardsProps> = ({ info }) => {
   const { width } = useWindowDimensions();
   return (
-    <div className={`${DashboardStyles.info} gap-0-75`}>
-      <div className={`${DashboardStyles.infoItem} shadow-sm w-100 bg-light round-sm text-dark
-        flex justify-space-between ${width >= breakpoints['xs'] ? 'p-1-25' : 'p-0-85'}`}>
+    <div
+      className={DashboardStyles.info}
+      style={{
+        gridGap: "0.75rem",
+      }}
+    >
+      <div
+        className={`${
+          DashboardStyles.infoItem
+        } shadow-sm w-100 bg-light round-sm text-dark
+        flex justify-space-between ${
+          width >= breakpoints["xs"] ? "p-1-25" : "p-0-85"
+        }`}
+      >
         <div className="h-100 flex row-gap-0-45 justify-flex-start align-flex-start">
-          <p className="flex align-flex-start font-xl weight-500">Welcome Back, Shaun!</p>
+          <p className="flex align-flex-start font-xl weight-500">
+            Welcome Back, Shaun!
+          </p>
         </div>
         <div className="flex justify-flex-end align-flex-end h-100">
           <img src={img} alt="..." className="w-100" />
         </div>
       </div>
-      {
-        info.map((i, index) => (
-          <InfoCard
-            key={`ic-${index}`}
-            info={i}
-          />
-        ))
-      }
+      {info.map((i, index) => (
+        <InfoCard key={`ic-${index}`} info={i} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default InfoCards;
